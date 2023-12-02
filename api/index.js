13915547c8899679b286ed7db426ca5cfd391b17
jono <rabadunk@gmail.com>
2023-12-03 00:44:51 +1300

    trying to add locally hosted image

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ let buildChart = async (activities) => {
 
 
     let svgChart = d3n.createSVG(500, 170);
-    svgChart.append('image').attr('href', "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Strava_Logo.svg/2560px-Strava_Logo.svg.png").attr("x", 10).attr("y", "-60").attr("height", 200).attr("width", 200);
+    svgChart.append('image').attr('href', "logo.png").attr("x", 10).attr("y", "-60").attr("height", 200).attr("width", 200);
     svgChart.append('text').text('Runs').attr('class', 'title').attr('x', 10).attr('y', 100).attr("font-family", "Arial, Helvetica, sans-serif");
     svgChart.append('text').text(`${total_runs_count}`).attr('class', 'count').attr('x', 10).attr('y', 150);
     svgChart.append('text').text('Distance').attr('class', 'title').attr('x', 120).attr('y', 100).attr("font-family", "Arial, Helvetica, sans-serif");
@@ -51,6 +51,15 @@ let getRefreshToken = async () => {
 
     return response;
 }
+
+let main = async () => {
+    let refreshToken = await getRefreshToken();
+    let activities = await getActivites(refreshToken);
+    let chart = await buildChart(activities);
+    console.log(chart);
+}
+
+main();
 
 module.exports = async (req, res) => {
     res.setHeader('Content-Type', 'image/svg+xml');
