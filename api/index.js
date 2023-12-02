a99e838126e77c3ee6cb7bd4b85a4de65490a08d
jono <rabadunk@gmail.com>
2023-12-02 22:32:42 +1300

    can get run totals now

--- api/index.js
+++ api/index.js
@@ -3,8 +3,19 @@ const D3Node = require('d3-node');
 require('dotenv').config();
 const auth_link = "https://www.strava.com/oauth/token";
 
-async function buildChart() {
+async function buildChart(activities) {
     const d3n = new D3Node({styles:'.blah{fill:#fddb53;}.bar:hover{fill:#a9a9a9;opacity: 0.6;cursor:pointer;}.bar {fill:#fddb53;animation: load 2.5s;}.title{font-family:helvetica;font-size:13px;}.label{font-family:helvetica;font-size:8px;}@keyframes load { from{y: 120px; fill:orange;}}'});
+
+    let total_runs_count = activities.all_run_totals.count;
+    let total_distance =  activities.all_run_totals.distance/1000;
+    let hours = parseInt(activities.all_run_totals.moving_time/3600);
+    let minutes = parseInt((activities.all_run_totals.moving_time/3600 - hours)*60);
+
+    console.log(total_runs_count);
+    console.log(total_distance);
+    console.log(hours);
+    console.log(minutes);
+
     let svgChart = d3n.createSVG(460, 170);
     svgChart.append('text').text('Cases by DHB').attr('class', 'title').attr('x', 176).attr('y', 20);
 
@@ -20,18 +31,13 @@ let getActivites = async (res) => {
       
     });
 
-    instance.get(`https://www.strava.com/api/v3/athletes/${process.env.STRAVA_USER_ID}/stats`)
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.log(error);
-    });
+    const { data: response } = await instance.get(`https://www.strava.com/api/v3/athletes/${process.env.STRAVA_USER_ID}/stats`);
+    return response;
 }
 
-function getRefreshToken(){
-        
-    axios.post(auth_link, {
+let getRefreshToken = async () => {
+
+    const { data: response } = await axios.post(auth_link, {
             headers: {
                 'Accept': 'application/json, text/plain, */*',
                 'Content-Type': 'application/json'},
@@ -39,14 +45,20 @@ function getRefreshToken(){
             client_secret: process.env.STRAVA_CLIENT_SECRET,
             refresh_token: process.env.STRAVA_REFRESH_TOKEN,
             grant_type: 'refresh_token'
-        })
-        .then(function (response) {
-            getActivites(response.data);
-        })
-        .catch(function (error) {
-            console.log(error.data);
         });
+
+    return response;
 }
+
+
+let main = async () => {
+    let refreshToken = await getRefreshToken();
+    let activities = await getActivites(refreshToken);
+    let chart = await buildChart(activities);
+}
+
+
+main();
 
 module.exports = async (req, res) => {
     res.setHeader('Content-Type', 'image/svg+xml');
